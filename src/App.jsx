45f8e2244f615f5add28fe3490c9cd7a30845480
swalehazarekarindss <swaleha.zarekari@ndsofttech.com>
2025-10-07 16:33:22 +0530

    react-starter

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import HelloWorld from './Component/HelloWorld'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+<HelloWorld/>
+    </div>
+       
+    </>
+  )
+}
+
+export default App
